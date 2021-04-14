0a541ee5152e5168bef90cd602db985f6cb7f519
Erik Gahner Larsen <erikgahner@gmail.com>
2021-04-14 22:54:15 +0100

    render book

--- book/generateBook.R
+++ book/generateBook.R
@@ -1,8 +1,14 @@
 library("tidyverse")
 library("bookdown")
 library("knitr")
+library("tidylog")
+library("pageviews")
+library("gvlma")
+library("equatiomatic")
+library("thesisdown") #remotes::install_github("ismayc/thesisdown")
+library("gtrendsR")
+library("trendyy")
 
 render_book('index.Rmd', 
             'all', 
             config_file = "_bookdown.yml")
-
